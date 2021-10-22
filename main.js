89185561bb58c50a9d0823d9578e61add4afca41
DennisTarik <dm499555@gmail.com>
2021-10-22 14:46:03 +0200

    ✨ add searchForm

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import { createHeaderElement } from "./lib/headerElement";
 import { createMainElement } from "./lib/mainElement";
 import { createFooterElement } from "./lib/footerElement";
 import { createMarioCard } from "./components/marioCard";
+import { fetchCharacters } from "./lib/fetchCharacter";
 
 function renderApp() {
   const appElement = document.querySelector("#app");
@@ -12,6 +13,11 @@ function renderApp() {
   const mainComponent = createMainElement();
 
   const marioCard = createMarioCard();
+
+  const characterCards = fetchCharacters(
+    "https://mario-kart-tour-api.herokuapp.com/api/v1/drivers"
+  );
+  console.log(characterCards);
 
   const footerComponent = createFooterElement();
 
