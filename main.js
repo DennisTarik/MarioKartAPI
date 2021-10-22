3cee2f1bfba2c0e94b339176b4cd936fb8e63a45
DennisTarik <dm499555@gmail.com>
2021-10-22 11:37:57 +0200

    ✨ add main.js

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 import "./style.css";
+import { createHeaderElement } from "./lib/headerElement";
+import { createMainElement } from "./lib/mainElement";
+import { createFooterElement } from "./lib/footerElement";
+
+function renderApp() {
+  const appElement = document.querySelector("#app");
+
+  const headerComponent = createHeaderElement();
+
+  const mainComponent = createMainElement();
+
+  const footerComponent = createFooterElement();
+
+  appElement.append(headerComponent, mainComponent, footerComponent);
+}
+
+renderApp();
